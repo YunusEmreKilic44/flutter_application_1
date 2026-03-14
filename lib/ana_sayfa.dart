c1e7043c5a486e291bfbe7dcd8a88ceac76a3f0e
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-03-14 17:04:27 +0300

    Image.network

--- lib/ana_sayfa.dart
+++ lib/ana_sayfa.dart
@@ -4,14 +4,8 @@ class AnaSayfa extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Image.asset(
-        "assets/genshin.jpg",
-        width: 350,
-        height: 350,
-        // color: Colors.red,
-        // colorBlendMode: BlendMode.dstIn,
-        alignment: Alignment.topLeft,
-        fit: BoxFit.cover,
+      body: Image.network(
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZOftRTn9fcf_YCa6bClb3A8rZb0BsLlyg-B36BEQBAMfRffPBZkeyfji1lJp9HaHZYzniYtzt0Th9yASaEdmKLAZdXxUn4KNe-hziCUSw&s=10",
       ),
     );
   }
