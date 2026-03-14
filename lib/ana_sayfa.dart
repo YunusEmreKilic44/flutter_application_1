5ec3578143cdf53329d15f6b92f4fbd33c80e96b
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-03-14 17:04:07 +0300

    Image.asset

--- lib/ana_sayfa.dart
+++ lib/ana_sayfa.dart
@@ -4,17 +4,14 @@ class AnaSayfa extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Row(
-        children: [
-          Container(width: 50, height: 50, color: Colors.red),
-          SizedBox(height: 10, width: 10),
-          Container(width: 50, height: 50, color: Colors.blue),
-          SizedBox(height: 20, width: 20),
-          Container(width: 50, height: 50, color: Colors.green),
-          SizedBox(width: 30, height: 30),
-          Container(width: 50, height: 50, color: Colors.green),
-        ],
+      body: Image.asset(
+        "assets/genshin.jpg",
+        width: 350,
+        height: 350,
+        // color: Colors.red,
+        // colorBlendMode: BlendMode.dstIn,
+        alignment: Alignment.topLeft,
+        fit: BoxFit.cover,
       ),
     );
   }
