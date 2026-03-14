317b0c69ae89c7ded958dc1216c9475600d35752
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-03-14 17:07:47 +0300

    CircleAvatar - AssetImage - NetworkImage

--- lib/ana_sayfa.dart
+++ lib/ana_sayfa.dart
@@ -4,8 +4,16 @@ class AnaSayfa extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Image.network(
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZOftRTn9fcf_YCa6bClb3A8rZb0BsLlyg-B36BEQBAMfRffPBZkeyfji1lJp9HaHZYzniYtzt0Th9yASaEdmKLAZdXxUn4KNe-hziCUSw&s=10",
+      body: CircleAvatar(
+        // backgroundImage: AssetImage("assets/genshin.jpg"),
+        backgroundImage: NetworkImage(
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZOftRTn9fcf_YCa6bClb3A8rZb0BsLlyg-B36BEQBAMfRffPBZkeyfji1lJp9HaHZYzniYtzt0Th9yASaEdmKLAZdXxUn4KNe-hziCUSw&s=10",
+        ),
+        radius: 150,
+        child: Text(
+          "Circle Avatar",
+          style: TextStyle(color: Colors.black, fontSize: 48),
+        ),
       ),
     );
   }
