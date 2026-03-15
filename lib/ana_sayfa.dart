baeb4d1f8063fdef1132a4fdfa6c1c6d39264526
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-03-15 11:35:10 +0300

    Expanded

--- lib/ana_sayfa.dart
+++ lib/ana_sayfa.dart
@@ -1,37 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/sehir.dart';
 
 class AnaSayfa extends StatelessWidget {
-  List<Sehir> _sehirler = [
-    Sehir("İstanbul", 34, 15900000),
-    Sehir("Ankara", 6, 5700000),
-    Sehir("İzmir", 35, 4400000),
-    Sehir("Bursa", 16, 3100000),
-    Sehir("Antalya", 7, 2600000),
-    Sehir("Konya", 42, 2290000),
-    Sehir("Adana", 1, 2270000),
-    Sehir("Şanlıurfa", 63, 2170000),
-    Sehir("Gaziantep", 27, 2150000),
-    Sehir("Kocaeli", 41, 2000000),
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: ListView.builder(
-        itemCount: _sehirler.length,
-        itemBuilder: _listeOgesiniOlustur,
-      ),
-    );
-  }
-
-  Widget _listeOgesiniOlustur(BuildContext context, int index) {
-    return Card(
-      child: ListTile(
-        title: Text(_sehirler[index].isim),
-        subtitle: Text(_sehirler[index].nufus.toString()),
-        trailing: Text(_sehirler[index].plakaKodu.toString()),
-        leading: Icon(Icons.location_city),
+      body: Column(
+        children: [
+          Text("Liste Başı: ", style: TextStyle(fontSize: 32)),
+          Expanded(
+            child: ListView(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 100,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 100,
+                  color: Colors.blue,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 100,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 100,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 100,
+                  color: Colors.blue,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 100,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 100,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 100,
+                  color: Colors.blue,
+                ),
+              ],
+            ),
+          ),
+          Text("Liste Sonu", style: TextStyle(fontSize: 32)),
+        ],
       ),
     );
   }
