722dbf5fbb0031d5e283b7d3c4483a8e9b82572f
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-03-15 12:16:31 +0300

    DropdownButton

--- lib/ana_sayfa.dart
+++ lib/ana_sayfa.dart
@@ -1,26 +1,53 @@
 import 'package:flutter/material.dart';
 
-class AnaSayfa extends StatelessWidget {
+class AnaSayfa extends StatefulWidget {
+  @override
+  State<AnaSayfa> createState() => _AnaSayfaState();
+}
+
+class _AnaSayfaState extends State<AnaSayfa> {
+  List<String> _sehirler = [
+    "İstanbul",
+    "Ankara",
+    "İzmir",
+    "Tekirdağ",
+    "Antalya",
+  ];
+
+  String _secilenSehir = "İstanbul";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(width: double.infinity, color: Colors.red),
-            flex: 1,
+      body: Center(
+        child: DropdownButton<String>(
+          value: _secilenSehir,
+          items: _sehirler.map((String sehir) {
+            return DropdownMenuItem<String>(value: sehir, child: Text(sehir));
+          }).toList(),
+          onChanged: _sehirDegistir,
+          icon: Icon(Icons.location_city),
+          iconSize: 36,
+          dropdownColor: Colors.amber,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+            fontStyle: FontStyle.italic,
           ),
-          Expanded(
-            child: Container(width: double.infinity, color: Colors.blue),
-            flex: 2,
-          ),
-          Expanded(
-            child: Container(width: double.infinity, color: Colors.green),
-            flex: 3,
-          ),
-        ],
+          underline: Container(height: 5, color: Colors.red),
+          itemHeight: 200,
+          isExpanded: true,
+        ),
       ),
     );
+  }
+
+  void _sehirDegistir(String? yeniSecilenSehir) {
+    setState(() {
+      if (yeniSecilenSehir != null) {
+        _secilenSehir = yeniSecilenSehir;
+      }
+    });
   }
 }
