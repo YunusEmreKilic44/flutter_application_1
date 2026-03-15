4280f6a8ee66e3a3319e40fd5a3137a2d8cbebb7
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-03-15 11:02:39 +0300

    ListView

--- lib/ana_sayfa.dart
+++ lib/ana_sayfa.dart
@@ -1,77 +1,17 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
-class AnaSayfa extends StatefulWidget {
-  @override
-  State<AnaSayfa> createState() => _AnaSayfaState();
-}
-
-class _AnaSayfaState extends State<AnaSayfa> {
-  double _sonuc = 0;
-
-  TextEditingController _boyController = TextEditingController();
-
-  TextEditingController _kiloController = TextEditingController();
-
+class AnaSayfa extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Vücut Kitle Indexi")),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Text(_sonuc.toStringAsFixed(2), style: TextStyle(fontSize: 48)),
-            SizedBox(height: 16),
-            TextField(
-              controller: _boyController,
-              decoration: InputDecoration(
-                label: Text("Boyunuz"),
-                suffixText: "m",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
-            ),
-            SizedBox(height: 16),
-            TextField(
-              controller: _kiloController,
-              decoration: InputDecoration(
-                label: Text("Kilonuz"),
-                suffixText: "kg",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: _hesapla,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text("Hesapla", style: TextStyle(fontSize: 24)),
-              ),
-            ),
-          ],
-        ),
+      appBar: AppBar(),
+      body: ListView(
+        children: [
+          Container(width: double.infinity, height: 100, color: Colors.red),
+          Container(width: double.infinity, height: 100, color: Colors.blue),
+          Container(width: double.infinity, height: 100, color: Colors.green),
+        ],
       ),
     );
-  }
-
-  void _hesapla() {
-    String boyText = _boyController.text.trim();
-    String kiloText = _kiloController.text.trim();
-
-    try {
-      double boy = double.parse(boyText);
-      double kilo = double.parse(kiloText);
-
-      setState(() {
-        _sonuc = kilo / (boy * boy);
-      });
-    } catch (e) {
-      print("Bir hata oluştu: ${e.toString()}");
-    }
   }
 }
