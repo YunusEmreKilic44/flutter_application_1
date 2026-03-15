46aac746715a672fa88ecf7b3d2430592cd27cee
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-03-15 11:16:37 +0300

    ListView.builder

--- lib/ana_sayfa.dart
+++ lib/ana_sayfa.dart
@@ -1,17 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/sehir.dart';
 
 class AnaSayfa extends StatelessWidget {
+  List<Sehir> _sehirler = [
+    Sehir("İstanbul", 34, 15900000),
+    Sehir("Ankara", 6, 5700000),
+    Sehir("İzmir", 35, 4400000),
+    Sehir("Bursa", 16, 3100000),
+    Sehir("Antalya", 7, 2600000),
+    Sehir("Konya", 42, 2290000),
+    Sehir("Adana", 1, 2270000),
+    Sehir("Şanlıurfa", 63, 2170000),
+    Sehir("Gaziantep", 27, 2150000),
+    Sehir("Kocaeli", 41, 2000000),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: ListView(
-        children: [
-          Container(width: double.infinity, height: 100, color: Colors.red),
-          Container(width: double.infinity, height: 100, color: Colors.blue),
-          Container(width: double.infinity, height: 100, color: Colors.green),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: _sehirler.length,
+          itemBuilder: _listeOgesiniOlustur,
+        ),
       ),
+    );
+  }
+
+  Widget _listeOgesiniOlustur(BuildContext context, int index) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(_sehirler[index].isim),
+        SizedBox(height: 8),
+        Text(_sehirler[index].nufus.toString()),
+        SizedBox(height: 32),
+      ],
     );
   }
 }
